f3a57f5c6ba00e8f4a1711c4f874a0cf63b83bf5
anders130 <93037023+anders130@users.noreply.github.com>
2024-12-23 15:55:37 +0100

    init flake

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+    description = "nixfox flake";
+
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+        flake-parts.url = "github:hercules-ci/flake-parts";
+    };
+
+    outputs = inputs:
+        inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+            systems = ["x86_64-linux"];
+            perSystem = {pkgs, ...}: {
+                packages.default = pkgs.hello;
+            };
+        };
+}
