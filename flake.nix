b4499e8648014c705ff3b2a357212d9ea1363c1c
anders130 <93037023+anders130@users.noreply.github.com>
2024-12-23 21:46:59 +0100

    add simple zen-wrapper for firefox hm-module

--- flake.nix
+++ flake.nix
@@ -4,11 +4,20 @@
     inputs = {
         nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
         flake-parts.url = "github:hercules-ci/flake-parts";
+        home-manager = {
+            url = "github:nix-community/home-manager";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
     };
 
-    outputs = inputs:
-        inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+    outputs = inputs @ {
+        flake-parts,
+        self,
+        ...
+    }:
+        flake-parts.lib.mkFlake {inherit inputs;} {
             systems = ["x86_64-linux"];
+            flake.nixosModules.default = import ./nixos-module.nix {inherit self;};
             perSystem = {pkgs, ...}: {
                 packages.default = pkgs.hello;
             };
---
+++ nixos-module.nix
@@ -0,0 +1,58 @@
+self: {
+    config,
+    lib,
+    ...
+}: let
+    cfg = config.nixfox;
+
+    generateIni = profiles: let
+        profileList = lib.mapAttrsToList (name: profile: profile // {inherit name;}) profiles;
+        profileToStr = profile: ''
+            [Profile${toString profile.id}]
+            Name=${profile.name}
+            IsRelative=1
+            Path=${profile.name}
+            ZenAvatarPath=chrome://browser/content/zen-avatars/avatar-55.svg
+            Default=${if profile.isDefault then "1" else "0"}
+        '';
+    in 
+        builtins.concatStringsSep "\n\n" ((map profileToStr profileList) ++ [''
+            [General]
+            StartWithLastProfile=1
+            Version=2
+        '']);
+in {
+    options.nixfox = {
+        enable = lib.mkEnableOption "nixfox";
+        package = lib.mkOption {
+            type = lib.types.package;
+            description = "Package for zen-browser";
+        };
+        profiles = lib.mkOption {
+            type = lib.types.attrsOf lib.types.anything;
+            default = {};
+            description = "This can be used like home-manager's firefox profiles";
+        };
+        username = lib.mkOption {
+            type = lib.types.str;
+        };
+    };
+
+    config = lib.mkIf config.nixfox.enable {
+        environment.systemPackages = [cfg.package];
+        home-manager.users.${cfg.username} = {
+            programs.firefox = {
+                enable = true;
+                profiles = lib.mapAttrs (name: profile: profile // {
+                    inherit name;
+                    # avoid conflicts with firefox profiles
+                    id = profile.id + 100;
+                    isDefault = false;
+                    # move to zen folder
+                    path = "../../.zen/${name}";
+                }) cfg.profiles;
+            };
+            home.file.".zen/profiles.ini".text = generateIni cfg.profiles;
+        };
+    };
+}
